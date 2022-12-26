baad54aabfd4f62023e3e69815aa07d453dd610d
m1172 <muhshuh2000@gmail.com>
2022-12-26 17:58:02 +0100

    Recursive Binary Search

--- code.js
+++ code.js
@@ -1,21 +1,22 @@
-function binarySearch(arr, target) {
-  let leftIndex = 0;
-  let rightIndex = arr.length - 1;
-
-  while (leftIndex <= rightIndex) {
-    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
-
-    if (target === arr[middleIndex]) {
-      return middleIndex;
-    }
-
-    if (target < arr[middleIndex]) {
-      rightIndex = middleIndex - 1;
-    } else {
-      leftIndex = middleIndex + 1;
-    }
-  }
-  return -1;
+function recursiveBinarySearch(arr, target) {
+  return Search(arr, target, 0, arr.length - 1);
 }
 
-console.log(binarySearch([-5, 2, 4, 6, 10], 2));
+function Search(arr, target, leftIndex, rightIndex) {
+  if (leftIndex > rightIndex) {
+    return -1;
+  }
+
+  let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+  if (target == arr[middleIndex]) {
+    return middleIndex;
+  }
+
+  if (target < arr[middleIndex]) {
+    return Search(arr, target, leftIndex, middleIndex - 1);
+  } else {
+    return Search(arr, target, middleIndex + 1, rightIndex);
+  }
+}
+
+console.log(recursiveBinarySearch([-1, 2, 4, 7, 9, 100], 4));
