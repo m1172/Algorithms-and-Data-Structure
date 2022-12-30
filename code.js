f927bbbb51ae038cd337f9d87d1dae26be495cc2
m1172 <muhshuh2000@gmail.com>
2022-12-30 12:18:09 +0100

    Insertion Sort

--- code.js
+++ code.js
@@ -1,18 +1,15 @@
-function bubleSort(arr) {
-  let swapped;
-  do {
-    swapped = false;
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        let temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        swapped = true;
-      }
+function incertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let numberToInsert = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > numberToInsert) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
     }
-  } while (swapped);
+    arr[j + 1] = numberToInsert;
+  }
 }
 
-const arr = [8, 20, -2, 4, -6];
-bubleSort(arr);
+const arr = [-8, 20, 2, 4, 6];
+incertionSort(arr);
 console.log(arr);
