79345134773d15e541e97391357557bf6c3f4877
m1172 <muhshuh2000@gmail.com>
2023-01-02 12:39:28 +0100

    Quick Sort

--- Algorithms.js
+++ Algorithms.js
@@ -216,3 +216,27 @@ incertionSort(arr);
 console.log(arr);
 
 // Big-O = O(n^2)
+
+// Quick Sort
+
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let pivot = arr[arr.length - 1];
+  console.log(pivot);
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  console.log(right);
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+// const arr = [8, 20, -2, 4, -6, 25];
+console.log(quickSort(arr));
--- code.js
+++ code.js
@@ -1,15 +1,20 @@
-function incertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let numberToInsert = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > numberToInsert) {
-      arr[j + 1] = arr[j];
-      j = j - 1;
-    }
-    arr[j + 1] = numberToInsert;
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
   }
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
-const arr = [-8, 20, 2, 4, 6];
-incertionSort(arr);
-console.log(arr);
+const arr = [8, 20, -2, 4, -6, 25];
+console.log(quickSort(arr));
